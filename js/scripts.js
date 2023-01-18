7cce0e589dbab2e54d7c687f25dea2964cf7864f
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-18 14:50:33 -0800

    added javascript logic

---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
+
+//buisness logic
+
+function wordCounter(text) {
+  if (isEmpty(text)) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function (element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  if (isEmpty(text)) {
+    return 0;
+  }
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function dontCountBadWords(text) {
+  const badWordArray = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+  const textArray = text.split(" ");
+  let goodArray = [];
+  textArray.forEach(function (word) {
+    if (!badWordArray.includes(word.toLowerCase())) {
+      goodArray.push(word);
+      
+    } 
+  });
+  return goodArray.join(' ');
+
+}
+
+
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const dontCount = dontCountBadWords(passage);
+  const wordCount = wordCounter(dontCount);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
+
+
